51be3273a3f729342f064530ac46edd96ae94771
Sumit Autade <128409006+Sumit-Autade@users.noreply.github.com>
2023-11-06 21:37:03 +0530

    Update 1_Price Predictor.py

--- pages/1_Price Predictor.py
+++ pages/1_Price Predictor.py
@@ -11,7 +11,8 @@ st.set_page_config(page_title="Viz Demo")
 with open('df.pkl','rb') as file:
     df = pickle.load(file)
 with open('pipeline.pkl','rb') as file:
-    df = pickle.load(file)
+    pipeline = pickle.load(file)
+
 
 
 
